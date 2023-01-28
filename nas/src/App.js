cfe590c2d7022dcf66443bbc41a3bc70e7e7d5b8
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-28 14:36:18 -0500

    added card + datepicker styling

--- nas/src/App.js
+++ nas/src/App.js
@@ -1,6 +1,6 @@
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
 function App() {
@@ -16,31 +16,40 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 className="font-bold mb-1 text-gray-700 block">
-          NASA's Picture of the Day
+        <h1 className="font-bold mb-1 text-gray-700 block text-center text-5xl">
+          NASA's picture of the day
         </h1>
       </div>
       <label
         htmlFor="datepicker"
-        className="font-bold mb-1 text-gray-700 block"
+        className="my-4 text-gray-700 block text-center text-lg"
       >
-        Select Date explore NASA's Picture of that Day.
+        Select a date to explore NASA's picture of that day.
       </label>
       <DatePicker
-        className="cursor-pointer pl-4 pr-10 py-3 leading-none rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-600 font-medium bg-slate-200"
+        className="cursor-pointer pl-4  py-3 leading-none rounded-lg shadow-sm hover:bg-slate-300 text-gray-600 font-medium bg-slate-200 "
         selected={startDate}
         onChange={(date) => setDate(date)}
         minDate={new Date("1995-06-17")}
         maxDate={new Date()}
       />
-      <img src={nasaData?.hdurl} alt="" />
-      <div>
-        <ul>
-          <li>
-            <h3>{nasaData?.title}</h3>
-            <h3>{nasaData?.explanation}</h3>
-          </li>
-        </ul>
+      <div class="max-w-3xl rounded overflow-hidden shadow-lg">
+        <img class="w-full" src={nasaData?.hdurl} alt={nasaData?.title} />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{nasaData?.title}</div>
+          <p class="text-gray-700 text-base">{nasaData?.explanation}</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -55,18 +64,18 @@ function FetchNasa(date, fn) {
     (date.getDate() > 9 ? date.getDate() : "0" + date.getDate());
 
   console.log(urlDate);
-  
-    fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_KEY}&date=${urlDate}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        fn(data);
-      })
-      .catch((err) => {
-        console.log(`error ${err}`);
-      });  
+
+  fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_KEY}&date=${urlDate}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      fn(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
 
 export default App;
